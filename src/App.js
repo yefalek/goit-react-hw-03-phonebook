1d29741199ac6357f9585cf102dad179bddb3444
yefalek <yefremenkov.alek@gmail.com>
2021-12-09 18:52:36 +0200

    add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Form from "./components/ContactForm/Form";
+import ContactsList from "./components/ContactsList/ContsctsList";
+import Filter from "./components/Filter/Filter";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    contacts: [
+      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+    ],
+  };
+
+  handleAddContact = (newContact) =>
+    this.setState(({ contacts }) => ({
+      contacts: [...contacts, newContact],
+    }));
+
+  handleCheckUniqueContact = (name) => {
+    const { contacts } = this.state;
+
+    const isExistContact = !!contacts.find((contact) => contact.name === name);
+
+    isExistContact && alert("Contact is already exist");
+
+    return !isExistContact;
+  };
+
+  handleRemoveContact = (id) =>
+    this.setState(({ contacts }) => ({
+      contacts: contacts.filter((contact) => contact.id !== id),
+    }));
+
+  handleFilterChange = (filter) => this.setState({ filter });
+
+  getVisibleContacts = () => {
+    const { contacts, filter } = this.state;
+    return contacts.filter((contact) => contact.name.includes(filter));
+  };
+
+  render() {
+    const { filter } = this.state;
+    const visibleContacts = this.getVisibleContacts();
+    return (
+      <>
+        <h2>Form Contact</h2>
+        <Form
+          className={"forms"}
+          onAdd={this.handleAddContact}
+          onCheckUnique={this.handleCheckUniqueContact}
+        />
+        <h2>Contacts list</h2>
+        <Filter filter={filter} onChange={this.handleFilterChange} />
+        <ContactsList
+          contacts={visibleContacts}
+          onRemove={this.handleRemoveContact}
+        />
+      </>
+    );
+  }
 }
 
 export default App;
